d9c1bacee6efedfd6888ddbc0a01003e7c3fffe2
Renan Ramos <renanrrj@icloud.com>
2025-05-22 10:56:45 -0300

    Atualização do xpath e correção da criação de nova aba com os titulos

--- Automacao.py
+++ Automacao.py
@@ -14,20 +14,27 @@ def iniciar_navegador():
 
 def acessar_speedtest(navegador):
     navegador.get('https://www.speedtest.net/pt')
-    WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[1]/a/span[4]')))
-    navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[1]/a/span[4]').click()
+    WebDriverWait(navegador, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'start-text')))    
+    navegador.find_element(By.CLASS_NAME, 'start-text').click()
     time.sleep(40)  # Espera até o teste ser concluído
 
 def obter_dados(navegador):
     Data = datetime.today().strftime('%Y-%m-%d')
-    Hora = datetime.now().time().strftime('%H:%M:%S')
-
-    Download = navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
-    Upload = navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-    Min = int(navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[2]/div/span[3]/span').text)
-    Max = int(navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[2]/div/span[4]/span').text)
-    Media = int(navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/div[3]/div/div[3]/div/div/div[2]/div[2]/div/span[2]/span').text)
-
+    Hora = datetime.now().time().strftime('%H:%M:%S')    
+    
+    WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'download-speed')))
+    WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'upload-speed')))    
+    WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'ping-speed')))
+    #WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'ping-min')))
+    #WebDriverWait(navegador, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, 'ping-max')))
+    
+    Download = navegador.find_element(By.CLASS_NAME, 'download-speed').text
+    Upload = navegador.find_element(By.CLASS_NAME, 'upload-speed').text 
+    Min = int(navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[4]/div/div[3]/div/div/div[2]/div[2]/div/span[3]/span').text)
+    Max = int(navegador.find_element(By.XPATH, '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[2]/div/div[4]/div/div[3]/div/div/div[2]/div[2]/div/span[4]/span').text)       
+    Media = int(navegador.find_element(By.CLASS_NAME, 'ping-speed').text)
+    
+    
     return Data, Hora, Download, Upload, Min, Max, Media
 
 def fechar_navegador(navegador):
--- Planilha.py
+++ Planilha.py
@@ -11,13 +11,14 @@ def carregar_ou_criar_planilha(caminho, MesPlanilha):
         plan = opx.load_workbook(filename=caminho)
         if MesPlanilha not in plan.sheetnames:
             pag_med = plan.create_sheet(MesPlanilha)
+            pag_med.append(['Data', 'Hora', 'Download', 'Upload', 'Min', 'Max', 'Med'])  # Adiciona cabeçalho
         else:
             pag_med = plan[MesPlanilha]
     except FileNotFoundError:
         plan = opx.Workbook()
         plan.active.title = MesPlanilha
         pag_med = plan.active
-        pag_med.append(['Data', 'Hora', 'Download', 'Upload', 'Min', 'Max', 'Med'])
+        pag_med.append(['Data', 'Hora', 'Download', 'Upload', 'Min', 'Max', 'Med'])  # Adiciona cabeçalho
 
     return plan, pag_med
 
